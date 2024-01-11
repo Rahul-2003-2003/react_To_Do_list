5ff5b7cd84ed10acad7bc7ab81fc92893a52134b
dines14-coder <150772284+dines14-coder@users.noreply.github.com>
2024-01-11 20:37:38 +0530

    Delete src directory

--- src/Redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import tasksReducers from './slices/Taskslice'
-
-
-export const store = configureStore({
-    reducer : {
-        tasks : tasksReducers
-    }
-})
